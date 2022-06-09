97adccf32d61f80613bbd6de15f2e498af6a5a87
thalials <thalials@al.insper.edu.br>
2022-06-08 23:05:55 -0300

    ajustes na visao do jogador

--- Assets/_Scripts/PlayerCam.cs
+++ Assets/_Scripts/PlayerCam.cs
@@ -22,7 +22,7 @@ public class PlayerCam : MonoBehaviour
 
         yRotation += mouseX;
         xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        xRotation = Mathf.Clamp(xRotation, -90f, 20f);
 
         transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
